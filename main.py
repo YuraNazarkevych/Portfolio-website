b336444e4a7e2f20a04c658d4c1e604f6855fe4b
YuraN <yuranazarkevych767@gmail.com>
2024-08-05 23:44:17 +0300

    Create columns, image and, description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Yuriy Nazarkevych")
+    content = """
+    Hi, I am Yura! I am a Python developer. 
+    This is my portfolio website with my projects.
+    I will add more information about myself in the future.
+    """
+    st.info(content)
